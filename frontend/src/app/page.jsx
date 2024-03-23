26a76e024ba68e320d55efa42298844359500a86
AmitBloodLine <bloodline90265@gmail.com>
2024-03-23 22:00:41 +0530

    home page added

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '@/components/Navbar';
+import Home1 from './pages/Home1';
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Home1 />
     </>
   )
 }
