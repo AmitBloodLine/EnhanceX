3a93c2dd34342921ee8ae45df1296706cfddc638
AmitBloodLine <bloodline90265@gmail.com>
2024-03-24 13:29:47 +0530

    error fixed

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Navbar from '@/components/Navbar';
-import Home1 from './pages/Home1';
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Home1 />
     </>
   )
 }
