1b7382cb46b910ff0d7ad02637addf8323b98f4a
AmitBloodLine <bloodline90265@gmail.com>
2024-03-29 00:20:33 +0530

    formValidation

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -80,12 +80,20 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                href={"/signup"}
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Signup
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/contact"}
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
