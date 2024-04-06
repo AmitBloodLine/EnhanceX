683505ca1a0c485511ffcd07e74632a7ea2f0c66
AmitBloodLine <bloodline90265@gmail.com>
2024-04-06 11:45:32 +0530

    11:45

---
+++ frontend/src/app/plugins/objectpage/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <div className='bg-auto bg-center h-96 object-cover'
+        style={
+            {backgroundImage: `url('https://media.hackerearth.com/blog/wp-content/uploads/2018/08/shutterstock_668209624-1.jpg')`,
+            backgroundSize: 'cover',
+            
+            }}
+        >
+            
+        </div>
+    </div>
+  )
+}
+
+export default page
