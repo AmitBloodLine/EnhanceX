7e51b90e264913ee3103f0a24154e67827be8009
AmitBloodLine <bloodline90265@gmail.com>
2024-04-09 14:09:24 +0530

    plugins

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
   });
   return (
     <div
-    className="h-screen bg-opacity-25"
+    className="h-screen bg-opacity-25 bg-cover"
         style={{
           backgroundImage: `url('https://images.unsplash.com/photo-1480506132288-68f7705954bd?q=80&w=2120&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
         }}>
