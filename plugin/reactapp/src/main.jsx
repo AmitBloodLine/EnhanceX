1d0a01c7af6e69fb9a861297a6208d0382d0af57
AmitBloodLine <bloodline90265@gmail.com>
2024-04-21 23:19:30 +0530

    speech recognition test failed.

--- plugin/reactapp/src/main.jsx
+++ plugin/reactapp/src/main.jsx
@@ -1,29 +1,36 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import './index.css'
+// // import ImageRecognition from './plugins/ImageRecognition.jsx';
+// // import GestureRecognition from './components/GestureRecognition.jsx';
+
+// // import 'regenerator-runtime';
+
+// // ReactDOM.createRoot(document.getElementById('root')).render(
+// //   // <React.StrictMode>
+// //     <App />
+// //   // </React.StrictMode>,
+// // )
+
+
+// const imageRecognitionEle = document.querySelector('#image-recognition-plugin');
+// if (imageRecognitionEle) {
+//   ReactDOM.createRoot(imageRecognitionEle).render(
+//     <ImageRecognition />
+//   )
+// }
+
+// const gestureRecognitionEle = document.querySelector('#gesture-recognition-plugin');
+// if (gestureRecognitionEle) {
+//   ReactDOM.createRoot(gestureRecognitionEle).render(
+//     <GestureRecognition />
+//   )
+// }
+
+import  'regenerator-runtime/runtime';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-// import ImageRecognition from './plugins/ImageRecognition.jsx';
-// import GestureRecognition from './components/GestureRecognition.jsx';
-
-// import 'regenerator-runtime';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <App />
-//   // </React.StrictMode>,
-// )
+import App from './App.jsx'
 
 
-const imageRecognitionEle = document.querySelector('#image-recognition-plugin');
-if (imageRecognitionEle) {
-  ReactDOM.createRoot(imageRecognitionEle).render(
-    <ImageRecognition />
-  )
-}
-
-const gestureRecognitionEle = document.querySelector('#gesture-recognition-plugin');
-if (gestureRecognitionEle) {
-  ReactDOM.createRoot(gestureRecognitionEle).render(
-    <GestureRecognition />
-  )
-}
-
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
