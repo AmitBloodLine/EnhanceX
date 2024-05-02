b5847ca2f7a1a05e7f6004d2f520572d0c5826aa
AmitBloodLine <bloodline90265@gmail.com>
2024-05-02 13:07:08 +0530

    texttoxicity model added

--- new-plugin/src/App.jsx
+++ new-plugin/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import SpeechRecognitionPlugin from './components/SpeechRecognition';
 import GestureRecognition from './components/GestureRecognition';
+import ModeratorPlugin from './components/ToxicityClassification';
 // import TextToxicity from './components/TextToxicity';
 
 const App = () => {
   return (
     <div>
       {/* <SpeechRecognitionPlugin /> */}
-      <GestureRecognition />
-      {/* <TextToxicity/> */}
+      {/* <GestureRecognition /> */}
+      <ModeratorPlugin/>
     </div> 
   )
 }
