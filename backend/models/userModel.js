43e876f8d08fb384590eae8601cc7c55728d94a3
m-triple-m <triplem656@gmail.com>
2024-05-10 09:57:50 +0530

    ..

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,60 @@
 const { Schema, model } = require('../dbconnection');
+const bcrypt = require("bcrypt");
+const SALT = 10;
 
 const mySchema = new Schema({
     username: { type: String, require: true },
     email: { type: String, require: true, unique: true },
     password: String,
     role: { type: String, default: 'user' },
+    avatar: { type: String, default: 'avatar_placeholder.png' },
     createdAt: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     },
 });
 
-module.exports = model( 'user', mySchema );
+mySchema.pre("save", function (next) {
+    let user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) {
+        console.log("Password was not modified");
+        return next();
+    }
+
+    // generate a salt
+    bcrypt.genSalt(SALT, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+mySchema.methods.comparePassword = function (candidatePassword, cb) {
+    console.log("comparing...", candidatePassword);
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        console.log(isMatch);
+        if (err) {
+            console.log(err);
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
+mySchema.methods.authenticate = function (formData, cb) {
+    // console.log('formData', formData);
+    bcrypt.compare(formData.password, this.password, function (err, isMatch) {
+        if (err && formData.email === this.email) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = model('user', mySchema);
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const Model = require('../models/userModel');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 router.post('/add', (req, res) => {
     console.log(req.body);
     new Model(req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get("/getall", (req, res) => {
     Model.find({})
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/update', (req, res) => {
@@ -29,27 +31,53 @@ router.get('/update', (req, res) => {
 
 router.delete('/delete/:id', (req, res) => {
     Model.findByIdAndDelete(req.params.id)
-    .then((result) => {
-        res.status(200).json(result)
-    }).catch((err) => {
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result)
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
-router.post('/authenticate',(req,res) => {
-   Model.findOne(req.body)
-   .then((result) => {
-    if(result){
-        res.json(result);
-    }else{
-        res.status(401).json({message: "Invalide Credential"})
-    }
-   }).catch((err) => {
-    console.log(err);
+router.post("/authenticate", (req, res) => {
+    Model.findOne({ email: req.body.email })
+        .then((result) => {
+            if (result) {
+                new Model(result).comparePassword(req.body.password, (err, isMatch) => {
+                    if (err || !isMatch) {
+                        console.error("Error authenticating user", err);
+                        res.status(500).send({ status: "failed" });
+                    } else {
 
-    res.status(500).json(err);
-   });
+                        const { _id, email, role, username, avatar } = result;
+                        const payload = { _id, email, role, username, avatar };
+
+                        jwt.sign(
+                            payload,
+                            process.env.JWT_SECRET,
+                            { expiresIn: '3 days' },
+                            (err, token) => {
+                                if (err) {
+                                    console.error("Error authenticating user", err);
+                                    res.status(500).send({ status: "failed" });
+                                } else {
+                                    console.log("User authenticated");
+                                    res.status(200).json({ token, username, role, avatar });
+                                }
+                            }
+                        )
+                    }
+                });
+            } else {
+                console.error("Error authenticating user");
+                res.status(501).json({ status: "failed" });
+            }
+        })
+        .catch((err) => {
+            console.error("Error authenticating user", err);
+            res.status(502).json({ status: "failed" });
+        });
 });
+
 
 module.exports = router;
 
