d714118cb2ad6c16a7513db0f3de41d92341d7a3
AmitBloodLine <bloodline90265@gmail.com>
2024-05-13 23:19:07 +0530

    fix ui

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,81 +4,81 @@ import React from "react";
 import useUserContext from "@/context/UserContext";
 
 const Navbar = () => {
-  const {loggedIn , logout} = useUserContext();
+  const { loggedIn, logout } = useUserContext();
   console.log(loggedIn);
 
   const showLoggedIn = () => {
-    if(loggedIn) {
-      return(
+    if (loggedIn) {
+      return (
         <div class="sm:order-3 flex items-center gap-x-2">
-             <button
+          <button
             type="button"
-            class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-red-200 bg-white text-white-600 shadow-sm hover:bg-red-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-900 dark:border-red-700 dark:text-white-500 dark:hover:bg-red-800"
+            class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-900 dark:border-neutral-700 dark:text-blue-500 dark:hover:bg-white-800"
             onClick={logout}
           >
             Logout
           </button>
         </div>
       )
-    }else{
-      return(
+    } else {
+      return (
         <div class="sm:order-3 flex items-center gap-x-2">
-        <button
-          type="button"
-          class="sm:hidden hs-collapse-toggle p-2.5 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10"
-          data-hs-collapse="#navbar-alignment"
-          aria-controls="navbar-alignment"
-          aria-label="Toggle navigation"
-        >
-          <svg
-            class="hs-collapse-open:hidden flex-shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+          <button
+            type="button"
+            class="sm:hidden hs-collapse-toggle p-2.5 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10"
+            data-hs-collapse="#navbar-alignment"
+            aria-controls="navbar-alignment"
+            aria-label="Toggle navigation"
           >
-            <line x1="3" x2="21" y1="6" y2="6" />
-            <line x1="3" x2="21" y1="12" y2="12" />
-            <line x1="3" x2="21" y1="18" y2="18" />
-          </svg>
-          <svg
-            class="hs-collapse-open:block hidden flex-shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            <svg
+              class="hs-collapse-open:hidden flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <line x1="3" x2="21" y1="6" y2="6" />
+              <line x1="3" x2="21" y1="12" y2="12" />
+              <line x1="3" x2="21" y1="18" y2="18" />
+            </svg>
+            <svg
+              class="hs-collapse-open:block hidden flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+          </button>
+
+          <Link
+            type="button"
+            class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-900 dark:border-neutral-700 dark:text-blue-500 dark:hover:bg-white-800"
+            href="/signup"
           >
-            <path d="M18 6 6 18" />
-            <path d="m6 6 12 12" />
-          </svg>
-        </button>
-        
-        <Link
-          type="button"
-          class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-900 dark:border-neutral-700 dark:text-blue-500 dark:hover:bg-white-800"
-          href="/signup"
-        >
-          Register
-        </Link>
-        <Link
-          type="button"
-          class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-900 dark:border-neutral-700 dark:text-blue-500 dark:hover:bg-white-800"
-          href="/login"
-        >
-          Login
-        </Link>
-      
-      </div>
+            Register
+          </Link>
+          <Link
+            type="button"
+            class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-900 dark:border-neutral-700 dark:text-blue-500 dark:hover:bg-white-800"
+            href="/login"
+          >
+            Login
+          </Link>
+
+        </div>
       )
     }
   }
@@ -95,9 +95,9 @@ const Navbar = () => {
             alt="AI EnhanceX"
           />
         </Link>
-         {
+        {
           showLoggedIn()
-         }
+        }
         <div
           id="navbar-alignment"
           class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:grow-0 sm:basis-auto sm:block sm:order-2"
@@ -124,7 +124,7 @@ const Navbar = () => {
             >
               About
             </Link>
-          
+
             <Link
               class="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500"
               href="/feedback"
